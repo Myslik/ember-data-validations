ad445d33051a6e56e579670ee59a30f313a43e68
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2013-01-08 08:09:07 +0100

    initial draft

---
+++ ember-data-validations.js
@@ -0,0 +1,37 @@
+﻿var get = Ember.get;
+
+DS.DefaultValidators = {
+    required: function (value, options) {
+        if (options.required == true) {
+            return !!value && 0 !== value.length;
+        } else return true;
+    }
+};
+
+DS.Validation = Ember.Object.create({
+    init: function () {
+        var defaults = DS.DefaultValidators;
+        this.validators = [];
+        for (var validator in defaults) {
+            if (defaults.hasOwnProperty(validator)) {
+                this.validators.push(defaults[validator]);
+            }
+        }
+    },
+    register: function (validator) {
+        this.validators.push(validator);
+    },
+    validate: function (value, options) {
+        return this.validators.reduce(function (previousValue, item) {
+            return previousValue && item(value, options);
+        }, true);
+    }
+});
+
+DS.Model.reopen({
+    validateProperty: function (name) {
+        var meta = get(this.constructor, 'attributes').get(name);
+        var value = get(this, name);
+        return DS.Validation.validate(value, meta.options);
+    }
+});
---
+++ tests/basic.js
@@ -0,0 +1,54 @@
+﻿var Person, store, array;
+
+var testSerializer = DS.JSONSerializer.create({
+    primaryKey: function () {
+        return 'id';
+    }
+});
+
+var TestAdapter = DS.Adapter.extend({
+    serializer: testSerializer
+});
+
+module("Basic", {
+    setup: function () {
+        store = DS.Store.create({
+            revision: 11,
+            adapter: TestAdapter.create()
+        });
+
+        Person = DS.Model.extend({
+            name: DS.attr('string', { required: true }),
+            isDrugAddict: DS.attr('boolean')
+        });
+    },
+
+    teardown: function () {
+        //Person = null;
+        //store = null;
+    }
+});
+
+test("model instance has validateProperty function", function () {
+    store.load(Person, { id: 1 });
+    var record = store.find(Person, 1);
+    ok(typeof record.validateProperty == 'function', "Passed!");
+});
+
+test("required validator returns true for non empty string", function () {
+    store.load(Person, { id: 1, name: 'Premysl Krajcovic' });
+    var record = store.find(Person, 1);
+    ok(record.validateProperty('name'), "Passed!");
+});
+
+test("required validator returns false for null value", function () {
+    store.load(Person, { id: 1 });
+    var record = store.find(Person, 1);
+    ok(!record.validateProperty('name'), "Passed!");
+});
+
+test("required validator returns false for empty string", function () {
+    store.load(Person, { id: 1, name: '' });
+    var record = store.find(Person, 1);
+    ok(!record.validateProperty('name'), "Passed!");
+});
