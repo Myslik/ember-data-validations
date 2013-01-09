025e9db40b23d65f43b87b0f82b16e347efd36c2
unknown <PremyslK@.CZ.moravia-it.com>
2013-01-09 16:55:47 +0100

    Automatic DS.Model validation

--- ember-data-validations.js
+++ ember-data-validations.js
@@ -1,10 +1,12 @@
 ﻿var get = Ember.get;
 
 DS.DefaultValidators = {
-    required: function (value, options) {
-        if (options.required == true) {
-            return !!value && 0 !== value.length;
-        } else return true;
+    required: function (value, meta) {
+        if (meta.options.required == true) {
+            if (!value || 0 === value.length) {
+                return "'%@' attribute is required".fmt(meta.name);
+            }
+        } else return '';
     }
 };
 
@@ -21,17 +23,46 @@ DS.Validation = Ember.Object.create({
     register: function (validator) {
         this.validators.push(validator);
     },
-    validate: function (value, options) {
-        return this.validators.reduce(function (previousValue, item) {
-            return previousValue && item(value, options);
-        }, true);
+    validate: function (value, meta) {
+        var length = this.validators.length;
+        for (var i = 0; i < length; i++) {
+            var validator = this.validators[i];
+            var error = validator(value, meta);
+            if (error) return error;
+        }
+        return '';
     }
 });
 
 DS.Model.reopen({
+    staleValidation: false,
+
     validateProperty: function (name) {
         var meta = get(this.constructor, 'attributes').get(name);
         var value = get(this, name);
-        return DS.Validation.validate(value, meta.options);
-    }
+        var error = DS.Validation.validate(value, meta);
+        if (error) {
+            this.errors = this.errors || {};
+            this.errors[name] = error;
+        }
+        return error;
+    },
+
+    validate: function () {
+        this.eachAttribute(function (name) {
+            this.validateProperty(name);
+        }, this);
+        return this.errors;
+    },
+
+    didChangeState: function () {
+        var state = get(this, 'stateManager.currentState');
+        if (state.name == 'uncommitted') {
+            if (state.parentState.name == 'updated') {
+                if (this.validate()) {
+                    this.send('becameInvalid');
+                }
+            }
+        }
+    }.observes('stateManager.currentPath')
 });
--- tests/basic.js
+++ tests/basic.js
@@ -19,36 +19,46 @@ module("Basic", {
 
         Person = DS.Model.extend({
             name: DS.attr('string', { required: true }),
+            age: DS.attr('number'),
             isDrugAddict: DS.attr('boolean')
         });
     },
 
     teardown: function () {
-        //Person = null;
-        //store = null;
+        Person = null;
+        store = null;
     }
 });
 
-test("model instance has validateProperty function", function () {
+test("model instance has been extended", function () {
     store.load(Person, { id: 1 });
     var record = store.find(Person, 1);
-    ok(typeof record.validateProperty == 'function', "Passed!");
+    ok(typeof record.validateProperty == 'function', "Model must have validateProperty function");
+    ok(typeof record.validate == 'function', "Model must have validateProperty function");
+});
+
+test("validate function on model correctly validates all attributes", function () {
+    store.load(Person, { id: 1, name: 'Premysl Krajcovic' });
+    var record = store.find(Person, 1);
+    ok(record.get('isValid'));
+    record.set('name', null);
+    ok(!record.get('isValid'));
 });
 
 test("required validator returns true for non empty string", function () {
     store.load(Person, { id: 1, name: 'Premysl Krajcovic' });
     var record = store.find(Person, 1);
-    ok(record.validateProperty('name'), "Passed!");
+    ok(!record.validateProperty('name'));
 });
 
 test("required validator returns false for null value", function () {
     store.load(Person, { id: 1 });
     var record = store.find(Person, 1);
-    ok(!record.validateProperty('name'), "Passed!");
+    ok(record.validateProperty('name'));
 });
 
 test("required validator returns false for empty string", function () {
     store.load(Person, { id: 1, name: '' });
     var record = store.find(Person, 1);
-    ok(!record.validateProperty('name'), "Passed!");
+    ok(record.validateProperty('name'));
 });
